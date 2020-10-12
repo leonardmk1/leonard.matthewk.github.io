90be158b41e492bda0849bb135f700ac5457eea5
Matthew Leonard <leonard.matthewk@gmail.com>
2020-10-12 15:33:23 -0400

    setup projects page

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -6,16 +6,28 @@ function Portfolio() {
     <div>
       <div className="project-container">
         <div className="project project-1">
-          <a href="/">Text here</a>
+          <a className="project-text" href="/">
+            Text here
+          </a>
+          <div className="project-img project-img-1"></div>
         </div>
         <div className="project project-2">
-          <a href="/">Text here</a>
+          <a className="project-text" href="/">
+            Text here
+          </a>
+          <div className="project-img project-img-2"></div>
         </div>
         <div className="project project-3">
-          <a href="/">Text here</a>
+          <a className="project-text" href="/">
+            Text here
+          </a>
+          <div className="project-img project-img-3"></div>
         </div>
         <div className="project project-4">
-          <a href="/">Text here</a>
+          <a className="project-text" href="/">
+            Text here
+          </a>
+          <div className="project-img project-img-4"></div>
         </div>
       </div>
     </div>
