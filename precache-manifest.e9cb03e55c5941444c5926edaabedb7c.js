fd60383ff279c5b5214763bb13160540ab83f2cb
Matthew Leonard <leonard.matthewk@gmail.com>
2020-10-21 09:00:10 -0400

    Updates

---
+++ precache-manifest.e9cb03e55c5941444c5926edaabedb7c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3758adf74d1aea5fad69e6c4e8674469",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d474a5b678b790029b32",
+    "url": "/static/css/main.5fa9ceb0.chunk.css"
+  },
+  {
+    "revision": "1473a3e4a0df6462b630",
+    "url": "/static/js/2.523967f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.523967f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d474a5b678b790029b32",
+    "url": "/static/js/main.203ef07e.chunk.js"
+  },
+  {
+    "revision": "31cc2c4e74a75ebaa9ea",
+    "url": "/static/js/runtime-main.2e1dc3e3.js"
+  },
+  {
+    "revision": "b65b77059a6c5baf0f6b4293bf904b92",
+    "url": "/static/media/Matt.b65b7705.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a254fc68c3d6f794ccb46ec0e853cb6d",
+    "url": "/static/media/project-1.a254fc68.jpeg"
+  },
+  {
+    "revision": "fc83f0286229cb8dc9a67f75243756a3",
+    "url": "/static/media/project-2.fc83f028.jpeg"
+  },
+  {
+    "revision": "838461ed8b7efa096ca8a090ec6ccdf8",
+    "url": "/static/media/project-3.838461ed.jpeg"
+  },
+  {
+    "revision": "18c76f2c0757aa1cb20aeaf53ab0cf84",
+    "url": "/static/media/project-4.18c76f2c.jpeg"
+  }
+]);
