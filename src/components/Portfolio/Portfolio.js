932c24b1065132130411f12e90929bb8096317f0
Matthew Leonard <leonard.matthewk@gmail.com>
2021-03-19 10:17:28 -0400

    updating styling on portfolio

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,54 +5,44 @@ function Portfolio() {
   return (
     <div>
       <div className="project-container">
-        <div className="project project-1">
-          <a
-            className="project-text"
-            target="blank"
-            href="http://moviestream1.herokuapp.com/"
-          >
-            Movie Stream
-          </a>
+        <a
+          className="project-text project project-1"
+          target="blank"
+          href="http://moviestream1.herokuapp.com/"
+        >
+          Movie Stream
           <div className="project-img project-img-1"></div>
-          <small className="project-bio">
-            Find Movies on Streaming Channels
-          </small>
-        </div>
-        <div className="project project-2">
-          <a
-            className="project-text"
-            target="blank"
-            href="https://reeledin.herokuapp.com/members"
-          >
-            ReeledIn
-          </a>
+          <p className="project-bio">Find Movies on Streaming Channels</p>
+        </a>
+        <a
+          className="project-text project project-2"
+          target="blank"
+          href="https://reeledin.herokuapp.com/members"
+        >
+          ReeledIn
           <div className="project-img project-img-2"></div>
-          <small className="project-bio">An app for fisherfolk</small>
-        </div>
-        <div className="project project-3">
-          <a
-            className="project-text"
-            target="blank"
-            href="https://leonardmk1.github.io/React-Employee-Directory-The-Office/"
-          >
-            The Office Employee Directory
-          </a>
+          <p className="project-bio">An app for fisherfolk</p>
+        </a>
+
+        <a
+          className="project-text project project-3"
+          target="blank"
+          href="https://leonardmk1.github.io/React-Employee-Directory-The-Office/"
+        >
+          The Office Employee Directory
           <div className="project-img project-img-3"></div>
-          <small className="project-bio">
-            Search and Sort Employee Directory
-          </small>
-        </div>
-        <div className="project project-4">
-          <a
-            className="project-text"
-            target="blank"
-            href="https://leonardmk1-express-handlebars.herokuapp.com/"
-          >
-            Burger App
-          </a>
+          <p className="project-bio">Search and Sort Employee Directory</p>
+        </a>
+
+        <a
+          className="project-text project project-4"
+          target="blank"
+          href="https://leonardmk1-express-handlebars.herokuapp.com/"
+        >
+          Burger App
           <div className="project-img project-img-4"></div>
-          <small className="project-bio">Node Express Handlebars</small>
-        </div>
+          <p className="project-bio">Node Express Handlebars</p>
+        </a>
       </div>
     </div>
   );
